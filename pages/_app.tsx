40e49c466e9eb862b671971a7032a4dbd3dd6f4a
guygreenleaf <guygreenleaf@icloud.com>
2021-12-11 23:10:51 -0800

    pd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
-
+//pd1
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
