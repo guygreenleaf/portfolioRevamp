7e3b8e8b731acf1ce18586f06f13a4bde86d1530
guygreenleaf <guygreenleaf@icloud.com>
2022-07-25 22:55:51 -0700

    Added contact email functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
+import 'react-quill/dist/quill.snow.css'
 import type { AppProps } from 'next/app'
+
 
 import { ChakraProvider } from '@chakra-ui/react'
 //pd1
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage} from 'next'
-import {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
 import profilePic from '../public/me.jpg'
 import styles from '../styles/Home.module.css'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Box } from '@chakra-ui/react'
 import { Stack } from '@chakra-ui/react'
 import { motion } from "framer-motion"
 import { Icon, IconButton } from '@chakra-ui/react'
@@ -14,8 +14,27 @@ import {AiFillCamera} from 'react-icons/ai';
 import {AiFillLinkedin} from 'react-icons/ai';
 import {RiNewspaperFill} from 'react-icons/ri';
 import {RiFolderOpenFill} from 'react-icons/ri';
+import { useToast } from '@chakra-ui/react'
 import cameraIco  from '../media/camera.png';
 import contract from '../media/contract.png'
+import email  from '../media/email.png';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button
+} from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+import { SMTPClient } from 'emailjs';
+import ReCAPTCHA from "react-google-recaptcha";
+import * as emailjs from 'emailjs-com'
+
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -32,10 +51,144 @@ const variantSpinner = {
 const Home: NextPage = () => {
 
   let [loadingCircle, showLoadingCircle] = useState(false);
+  const [emailMessage, setEmailMessage] = useState('');
+  const [isModalOpen, changeModalOpen] = useState(false);
+  const [isVerified, setIsVerified] = useState(false);
+  const [from, setFrom] = useState('');
+  const [fromEmail, setFromEmail] = useState('');
+  let [validationErrors, setValidationErrors] = useState<string[]>([]);
 
-  let pageLoading = () => {
-    showLoadingCircle(true);
+  const recaptchaRef:any = React.createRef();
+
+  const [isPreviewing, setPreviewing] = useState(false);
+
+  let previewChangeHandler = () => {
+
+    if(isPreviewing === false){
+      setValidationErrors([]);
+      if(verifyEmail() === true){
+        setPreviewing(true);
+      }
+      return;
+    }   
+
+    recaptchaRef.current.reset();
+    resetState();
+    setPreviewing(false);
   }
+
+  let modalChangeHandler = () => {
+    changeModalOpen(!isModalOpen);
+  }
+
+  let onModalClose = () => {
+    setEmailMessage('');
+    changeModalOpen(false);
+  }
+
+  let nameChangeHandler = (e:any) => {  
+    e.preventDefault();
+    setFrom(e.target.value);
+  }
+
+  let emailChangeHandler = (e:any) => {  
+    e.preventDefault();
+    setFromEmail(e.target.value);
+  }
+
+  let resetState = () => {
+    setIsVerified(false);
+    setFrom('');
+    setFromEmail('');
+    setEmailMessage('');
+  }
+
+  const onReCAPTCHAChange = (captchaCode:any) => {
+    if(!captchaCode) {
+      return;
+    }
+    setIsVerified(true);
+    recaptchaRef.current.reset();
+  }
+
+  let verifyEmail = ():boolean => {
+    var isValid = true;
+
+    setValidationErrors([]);
+
+    if(emailMessage.length === 0){
+      setValidationErrors(curr => [...curr, "Please enter a message."]);
+      isValid = false;
+    }
+    if(from.length === 0){
+      setValidationErrors(curr => [...curr, "Please enter your name."]);
+      isValid = false;
+    }
+    if(fromEmail.length === 0 || !fromEmail.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )){
+        setValidationErrors(curr => [...curr, "Please enter a valid email address."]);
+      isValid = false;
+    }
+    return isValid;
+  };
+
+  const toast:any = useToast()
+
+  function sendEmail() {
+    const templateParams = {
+      from_name: from,
+      from_email: fromEmail,
+      to_name: 'Guy Greenleaf',
+      message: emailMessage,
+    }
+
+    if(isVerified && verifyEmail()===true) {
+      console.log("AAA!!!")
+      emailjs.send(
+        process.env.NEXT_PUBLIC_SERVICE_ID ?? 'error',
+        process.env.NEXT_PUBLIC_TEMPLATE_ID ?? 'error',
+        templateParams,
+        process.env.NEXT_PUBLIC_EMAILJS_USER_ID
+      )
+        .then((result:any) => {
+          if(result.text === "OK"){
+            
+            resetState();
+            setPreviewing(false);
+            changeModalOpen(false);
+
+            toast({
+              title: 'Email Sent!',
+              description: "I'll reach out to you soon!",
+              status: 'success',
+              duration: 9000,
+              isClosable: true,
+            })
+          }
+         
+        }, (error:any) => {
+          toast({
+            title: `Error sending email - ${error}.`,
+            description: "Please Contact Me - contact@guygreenleaf.com.",
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        });
+      } else {
+        validationErrors.forEach(element => {
+          toast({
+            title: 'Error Sending Email.',
+            description: `Error - ${element}`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+          })
+        });
+      }
+  }
+
 
   if(loadingCircle){return (<div className={styles.containerDiv}>
    <motion.div
@@ -161,7 +314,6 @@ const Home: NextPage = () => {
             </div>
 
 
-
             <Link  href="https://publicfilesggreenleaf.s3.us-west-1.amazonaws.com/resume21.pdf" passHref>
             <a target="_blank">
               <motion.div
@@ -176,7 +328,6 @@ const Home: NextPage = () => {
                   <div className={styles.cardBlogContainer}>
                     <div>
                       <Image src={contract} alt="ResumeIcon" height="40px" width="40px">
-
                       </Image>
                     </div>
                     <div className={styles.cardBlogText}>  
@@ -188,13 +339,36 @@ const Home: NextPage = () => {
               </a>
             </Link>
 
+            <div onClick={() => modalChangeHandler()}>           
+              <motion.div
+              variants={variants} // Pass the variant object into Framer Motion 
+              initial="hidden" // Set the initial state to variants.hidden
+              animate="enter" // Animated state to variants.enter
+              exit="exit" // Exit state (used later) to variants.exit
+              transition={{ type: 'linear', duration: 1.5 }} // Set the transition to linear
+              className=""
+              >
+                <Flex direction="column" align="center" justify="center">
+                  <div className={styles.cardBlogContainer}>
+                    <div style={{marginBottom: '5px'}}>
+                      <Image src={email} alt="FlickerLogo" height="40px" width="40px">
+                      </Image>
+                    </div>
+                    <div className={styles.cardBlogText}>  
+                    Contact
+                    </div>
+                  </div>
+                </Flex>
+              </motion.div>     
+            </div>
+
 
             <motion.div
                 variants={variants} // Pass the variant object into Framer Motion 
                 initial="hidden" // Set the initial state to variants.hidden
                 animate="enter" // Animated state to variants.enter
                 exit="exit" // Exit state (used later) to variants.exit
-                transition={{ type: 'linear', duration: 1.50 }} // Set the transition to linear
+                transition={{ type: 'linear', duration: 1.75 }} // Set the transition to linear
                 className=""
             >
               <Flex direction="row" align="center" justify="center">
@@ -224,7 +398,70 @@ const Home: NextPage = () => {
                 </div>
               </Flex>
           </motion.div>
-        </Stack>
+
+      <Modal isOpen={isModalOpen} onClose={onModalClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+
+          {isPreviewing===false ? 
+          <div>
+            <ModalHeader>Send Me An Email</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            <div style={{marginBottom:'1rem'}}>
+              {validationErrors.length > 0 && (<h5 style={{textDecoration:"underline", fontWeight:"bold", color:'red'}}>Please resolve the following:</h5>)}           
+              {validationErrors.map((err:string, index:number) => {return (<p style={{color:'red'}} key={index}>{err}</p>)})}
+            </div>
+            <div style={{display:"flex", flexDirection:"row", marginBottom:'1rem'}}>
+              <div style={{width:'5rem'}}><span style={{float:'right'}}>Your Name</span></div>
+              <input type="text" placeholder='Your Name' style={{marginLeft:'1rem', border:'1px solid black', borderRadius:'4px', width:'80%'}} value={from} onChange={nameChangeHandler}></input>
+            </div>
+            <div style={{display:"flex", flexDirection:"row", marginBottom:'1rem'}}>
+              <div style={{width:'5rem'}}><span style={{float:'right'}}>Your Email</span></div>
+              <input type="text" placeholder='Your Email' style={{marginLeft:'1rem', border:'1px solid black', borderRadius:'4px', width:'80%'}} value={fromEmail} onChange={emailChangeHandler}></input>
+            </div>
+              <div style={{"height": "18rem"}}>
+                {/* @ts-ignore */}
+                <ReactQuill theme="snow" value={emailMessage} onChange={setEmailMessage} style={{"height": "15rem"}}/>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme='green' mr={0} onClick={() => previewChangeHandler()}>Preview & Send</Button>
+            </ModalFooter>
+          </div> 
+          :   
+          <div>
+            <ModalHeader>Send Me An Email</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>        
+              <div style={{border:"solid 1px black", borderRadius: "7px"}}>
+                {/* @ts-ignore */}
+                <ReactQuill
+                  value={emailMessage}
+                  readOnly={true}
+                  theme={"bubble"}
+                />
+              </div>
+              <div>          
+                <ReCAPTCHA
+                    ref={recaptchaRef}
+                    sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ? process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY : 'error'}                   
+                    onChange={onReCAPTCHAChange}
+                    
+                  />
+              </div>
+              <Box mt="4">
+                </Box>
+            </ModalBody>           
+            <ModalFooter>
+              <Button colorScheme='yellow' mr={3} onClick={() => previewChangeHandler()}>Back</Button>
+              <Button colorScheme='green' mr={0} onClick={sendEmail}>Send</Button>
+            </ModalFooter>
+          </div>
+          }        
+        </ModalContent>
+      </Modal>
+    </Stack>
   )
 }
 
