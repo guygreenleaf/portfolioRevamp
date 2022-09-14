b88a91558a9456700921c3b23b3b2d1351b91e54
guygreenleaf <guygreenleaf@icloud.com>
2022-09-13 18:57:09 -0700

    moved middleware

--- _middleware.ts
+++ _middleware.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const Middleware = (req: NextRequest) => {
+const middleware = (req: NextRequest) => {
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
     return NextResponse.next();
 
   return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
 };
 
-export default Middleware;
+export default middleware;
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/(r|R)(e|E)(s|S)(u|U)(m|M)(e|E)',
+  matcher: '/Resume',
 }
