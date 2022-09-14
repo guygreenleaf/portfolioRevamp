05ebad559bafaaa1710f6a6ddca38491662d616b
guygreenleaf <guygreenleaf@icloud.com>
2022-09-13 18:35:09 -0700

    updated tsconfig

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,7 +1,7 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from 'next/head'
 
-const resume: NextPage = ({resumeURI}: any) => {  
+let resume: NextPage = ({resumeURI}: any) => {  
     return(
         <div>
             <Head><title>Résumé</title></Head>
@@ -14,12 +14,12 @@ const resume: NextPage = ({resumeURI}: any) => {
 
 export default resume;
 
-export const getStaticProps: GetStaticProps = async(context) => {
+export let getStaticProps: GetStaticProps = async(context) => {
     var blobLink:string  = "";
     var uri = "";
     
-    const https = require("https");
-    const agent = new https.Agent({
+    let https = require("https");
+    let agent = new https.Agent({
       rejectUnauthorized: false
     })
 
