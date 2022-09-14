1b925217f9a4711e065d8353059362df8f5178e9
guygreenleaf <guygreenleaf@icloud.com>
2022-09-13 18:26:30 -0700

    ignoring case for resume route

--- _middleware.ts
+++ _middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const Middleware = (req) => {
+const Middleware = (req: NextRequest) => {
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
     return NextResponse.next();
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites(){
+    return [{
+      source: "/(r|R)(e|E)(s|S)(u|U)(m|M)(e|E)",
+      destination: "/resume"
+    }]
+  },
   images: {
     domains: ['i.imgur.com']
   },
