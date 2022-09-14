dd55d3d248d69caed0525e47e9942ee71eec6d16
guygreenleaf <guygreenleaf@icloud.com>
2022-09-13 18:36:34 -0700

    updated tsconfig

--- _middleware.ts
+++ _middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const Middleware = (req: NextRequest) => {
+let Middleware = (req: NextRequest) => {
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase())
     return NextResponse.next();
 
